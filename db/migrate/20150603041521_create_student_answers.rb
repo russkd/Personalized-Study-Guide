b8629a3bf8205c2e5470201b31e70329398db30f
russkd <russ.dollinger@ingenuitor.com>
2015-06-04 16:59:59 -0700

    Change reference to quizquestions

--- db/migrate/20150603041521_create_student_answers.rb
+++ db/migrate/20150603041521_create_student_answers.rb
@@ -2,7 +2,7 @@ class CreateStudentAnswers < ActiveRecord::Migration
   def change
     create_table :student_answers do |t|
       t.references :student, index: true, foreign_key: true
-      t.references :quiz_questions, index: true, foreign_key: true
+      t.references :quizquestions, index: true, foreign_key: true
       t.references :question_answers, index: true, foreign_key: true
 
       t.timestamps null: false
